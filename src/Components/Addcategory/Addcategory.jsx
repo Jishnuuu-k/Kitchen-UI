935115f019df8a3f485eadd36a598d9e9a9551a7
Jishnuuu-k <krishnakumarodappanal@gmail>
2025-02-19 11:15:17 +0530

    Aligned

--- src/Components/Addcategory/Addcategory.jsx
+++ src/Components/Addcategory/Addcategory.jsx
@@ -1,30 +1,39 @@
-import React from 'react';
-import './addcategory.css';
+import React from "react";
+import "./addcategory.css";
 
 function Addcategory({ sidebarWidth }) {
   return (
     <div className="MainCategoryContainer" style={{ marginLeft: sidebarWidth }}>
       <div className="Space-box"></div>
       <div className="Space-box"></div>
+
       <div className="CategoryContentBox">
         <form className="CategoryForm">
           <div className="CategoryFormHeading">
             <h2>Add Category</h2>
           </div>
+
           <div className="CategoryFormData">
             <div className="CategoryInputWrapper">
+              {/* Category Name */}
               <div className="CategoryInput">
                 <label>Category Name</label>
-                <input type="text" className="CategoryInputBox" placeholder="Enter category name" />
+                <input
+                  type="text"
+                  className="CategoryInputBox"
+                  placeholder="Enter category name"
+                />
               </div>
 
+              {/* Image Upload */}
               <div className="CategoryInput">
                 <label>Image</label>
                 <div className="Categoryinputboxx-wrapper">
-                    <input type="file" className="CategoryInputBoxx" />
+                  <input type="file" className="CategoryInputBoxx" />
                 </div>
               </div>
 
+              {/* Buttons */}
               <div className="CategoryInput">
                 <div className="CategoryButtonGroup">
                   <button className="CategoryAddButton">Add Category</button>
